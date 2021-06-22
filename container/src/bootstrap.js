850d3f12ee635710ed92db62ac7a54552c3b7ff4
Nati Kamusher <natan.kamusher@gmail.com>
2021-06-22 12:33:07 +0300

    completed developing container micro front end as well as cart and products

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,3 +1,7 @@
-import 'products/ProductsIndex'
-import 'cart/CartShow'
-console.log('Container!')
+import { mount as productsMount } from 'products/ProductsIndex';
+import { mount as cartMount } from 'cart/CartShow';
+
+console.log('Container!');
+
+productsMount(document.querySelector('#myproducts'));
+cartMount(document.querySelector('#mycart'));
